60e7297ec0241a400dc14792b34148762abf37ac
Favre Anael <anael.favre@gmail.com>
2018-02-12 16:33:04 +0100

    Adding value casting configuration

--- config/apifiltering.php
+++ config/apifiltering.php
@@ -15,5 +15,27 @@ return [
         'order_by' => [],
         'limit' => 100,
         'offset' => null,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Values casting
+    |--------------------------------------------------------------------------
+    |
+    | As the filtering from url is generally string we can cast values on the
+    | fly with the following array. You can, remove, edit or add values to be
+    | casted.
+    |
+    | Some casts are required for working well with SQL. For example 'null'
+    | should be parsed for making Where NULL/NOT NULL queries.
+    |
+    */
+
+    'casting' => [
+        [true, 1],
+        [false, 0],
+        ['true', 1],
+        ['false', 0],
+        ['null', null],
     ]
 ];
